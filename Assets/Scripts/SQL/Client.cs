c204d31efc3069be96ca285dc7c68e3a0ef18417
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-03-29 15:27:38 +0900

    SeverManage스크립트 분활(클라이언트,서버)
    - 각 역할에 맞게 스크립트 분활함

---
+++ Assets/Scripts/SQL/Client.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public GameObject PlayerCharctor; //이계정 플레이어 게임오브젝트를 담아둔 게임오브젝트
+    public GameObject PlayerPrefab;// 이계정에 접속한 플레이어를 나타내는 게임오브젝트 프리팹
+    public GameObject MonsterPrefab; // 몬스터 프리팹
+
+    public AllMonsterData ClientMonsterData; // 모든 몬스터 정보를 담아둔 클래스
+
+    public List<GameObject> Client_MonsterList = new List<GameObject>();// 모든 인게임존재하는 몬스터를 담아둔 클래스
+
+    [HideInInspector] public Maria5 maria = new Maria5("220.149.12.209", "OpenWorld_MapData", "nagne", "123");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerDefaultChecking()
+    {
+        DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Player_Data Where Player_ID = '" + SystemInfo.deviceUniqueIdentifier + "\'");
+        if (ds_json.Tables[0].Rows.Count == 0)
+        {
+            maria.create_Table(string.Format("INSERT INTO Player_Data(Player_ID) VALUES({0})", "\'" + SystemInfo.deviceUniqueIdentifier + "\'"));
+        }
+        else
+        {
+            PlayerCharctor = Instantiate(PlayerPrefab, PlayerPrefab.transform.position, Quaternion.identity);
+            PlayerCharctor.AddComponent<PlayerInfo>();
+            PlayerCharctor.name = JsonUtility.FromJson<PlayerClass>(ds_json.Tables[0].Rows[0]["data"].ToString()).Player_name;
+            PlayerCharctor.transform.position = JsonUtility.FromJson<PlayerClass>(ds_json.Tables[0].Rows[0]["data"].ToString()).PlayerPos;
+        }
+
+        StartCoroutine(Client_UpdateAllData());
+    }
+
+    public IEnumerator Client_UpdateAllData()
+    {
+        do
+        {
+            //DownLoad
+            DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Map1_Monster_Data Where Map_Num = '1'");
+            ClientMonsterData = JsonUtility.FromJson<AllMonsterData>(ds_json.Tables[0].Rows[0]["data"].ToString());
+            yield return new WaitForSecondsRealtime(0.1f);
+
+            //Upload
+            string send_json = JsonUtility.ToJson(new PlayerClass(PlayerCharctor));
+            maria.create_Table(string.Format("UPDATE Player_Data SET data = \'{0}\' WHERE Player_ID = \'{1}\'", send_json, SystemInfo.deviceUniqueIdentifier));
+
+        } while (true);
+    }
+
+    public void ClientDefaultSetting()
+    {
+        DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Map1_Monster_Data Where Map_Num = '1'");
+        ClientMonsterData = JsonUtility.FromJson<AllMonsterData>(ds_json.Tables[0].Rows[0]["data"].ToString());
+        for (int i = 0; i < ClientMonsterData.data.Count; i++)
+        {
+            GameObject tempObject = Instantiate(MonsterPrefab, MonsterPrefab.transform.position, Quaternion.identity);
+            tempObject.name = ClientMonsterData.data[i].Mob_name;
+            tempObject.transform.position = ClientMonsterData.data[i].MobPostion;
+            tempObject.AddComponent<MonsterControl>();
+            Client_MonsterList.Add(tempObject);
+        }
+
+        StartCoroutine(ClientMonsterUpdate());
+        StartCoroutine(ClientUpdateMonsterPotion());
+    }
+
+    public IEnumerator ClientMonsterUpdate()
+    {
+        do
+        {
+            yield return new WaitForSecondsRealtime(1f);
+            for (int i = 0; i < ClientMonsterData.data.Count; i++)
+            {
+                if (Client_MonsterList.Find(x => x.gameObject.name == ClientMonsterData.data[i].Mob_name) == null)
+                {
+                    GameObject tempObject = Instantiate(MonsterPrefab, MonsterPrefab.transform.position, Quaternion.identity);
+                    tempObject.name = ClientMonsterData.data[i].Mob_name;
+                    tempObject.transform.position = ClientMonsterData.data[i].MobPostion;
+                    tempObject.AddComponent<MonsterControl>();
+                    Client_MonsterList.Add(tempObject);
+                }
+            }
+
+        } while (true);
+    }
+    public IEnumerator ClientUpdateMonsterPotion()
+    {
+        do
+        {
+            yield return new WaitForSecondsRealtime(0.05f);
+            for (int i = 0; i < ClientMonsterData.data.Count; i++)
+            {
+                Client_MonsterList.Find(x => x.gameObject.name == ClientMonsterData.data[i].Mob_name).transform.position = ClientMonsterData.data[i].MobPostion;
+            }
+        } while (true);
+    }
+}
---
+++ Assets/Scripts/SQL/Server.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    [HideInInspector] public Maria5 maria = new Maria5("220.149.12.209", "OpenWorld_MapData", "nagne", "123");//서버인증
+
+    public GameObject PlayerPrefab; //서버에 표시할 플레이어들의 기본오브젝트 프리팹
+
+    public List<GameObject> Server_PlayerList = new List<GameObject>(); // 서버에서 가지고온 모든 플레이어 오브젝트 리스트
+
+    public AllPlayerData SeverAllPlayerData; //서버에서 가져온 모든 플레이어 정보를 담아둔곳
+    public AllMonsterData SeverMonsterData;// 서버에 올리는 모든 몬스터 정보를 담아둔 곳
+
+    public MonsterInfo[] MonsterList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SeverDefaultSetting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SeverDefaultSetting()
+    {
+        MonsterList = FindObjectsOfType<MonsterInfo>();
+        foreach (MonsterInfo temp in MonsterList)
+        {
+            SeverMonsterData.data.Add(new DefaultMobClass(temp.gameObject));
+        }
+        string send_json = JsonUtility.ToJson(SeverMonsterData);
+        maria.create_Table(string.Format("UPDATE {0} SET {1} = {2} WHERE {3} = {4} ", "Map1_Monster_Data", "data", "\'" + send_json + "\'", "Map_Num", "\'" + 1 + "\'"));
+
+        StartCoroutine(Server_UpdateAllData());
+        StartCoroutine(ServerPostion_Control());
+        StartCoroutine(Server_PlayerPostion_Control());
+    }
+
+    public IEnumerator Server_UpdateAllData()
+    {
+        do
+        {
+            //UpLoad
+            yield return new WaitForSecondsRealtime(0.1f);
+            //DownLoad
+            SeverAllPlayerData.data.Clear();
+            DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Player_Data");
+            foreach (DataRow temp in ds_json.Tables[0].Rows)
+            {
+                SeverAllPlayerData.data.Add(JsonUtility.FromJson<PlayerClass>(temp["data"].ToString()));
+            }
+
+        } while (true);
+    }
+
+    public IEnumerator ServerPostion_Control()
+    {
+        do
+        {
+            SeverMonsterData.data.Clear();
+            yield return new WaitForSecondsRealtime(0.1f);
+            foreach (MonsterInfo temp in MonsterList)
+            {
+                SeverMonsterData.data.Add(new DefaultMobClass(temp.gameObject));
+            }
+            string send_json = JsonUtility.ToJson(SeverMonsterData);
+            maria.create_Table(string.Format("UPDATE {0} SET {1} = {2} WHERE {3} = {4} ", "Map1_Monster_Data", "data", "\'" + send_json + "\'", "Map_Num", "\'" + 1 + "\'"));
+        } while (true);
+    }
+
+    public IEnumerator Server_PlayerPostion_Control()
+    {
+        DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Player_Data");
+        foreach (DataRow temp in ds_json.Tables[0].Rows)
+        {
+            SeverAllPlayerData.data.Add(JsonUtility.FromJson<PlayerClass>(temp["data"].ToString()));
+        }
+        Debug.Log(SeverAllPlayerData.data.Count);
+        for (int i = 0; i < SeverAllPlayerData.data.Count; i++)
+        {
+            GameObject tempObject = Instantiate(PlayerPrefab, PlayerPrefab.transform.position, Quaternion.identity);
+            tempObject.name = SeverAllPlayerData.data[i].Player_name;
+            tempObject.transform.position = SeverAllPlayerData.data[i].PlayerPos;
+            Server_PlayerList.Add(tempObject);
+        }
+
+        do
+        {
+            yield return new WaitForSecondsRealtime(0.1f);
+            for (int i = 0; i < SeverAllPlayerData.data.Count; i++)
+            {
+                if (Server_PlayerList.Find(x => x.gameObject.name == SeverAllPlayerData.data[i].Player_name))
+                {
+                    Server_PlayerList.Find(x => x.gameObject.name == SeverAllPlayerData.data[i].Player_name).gameObject.transform.position = SeverAllPlayerData.data[i].PlayerPos;
+                }
+            }
+
+
+        } while (true);
+
+    }
+}
