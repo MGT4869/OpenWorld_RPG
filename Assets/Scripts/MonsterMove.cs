336c367ebe9472f1dc07a800f970eac258fd6361
anhyeongjong <65288275+anhyeongjong@users.noreply.github.com>
2022-03-31 17:13:41 +0900

    캐릭터, 몬스터
    몬스터 이동 스크립트 수정
     * 가장 가까운 캐릭터로 이동
     * 캐릭터가 없을 때 자기 자리로 이동
    
    캐릭터
     * 애니메이터 완성

--- Assets/Scripts/MonsterMove.cs
+++ Assets/Scripts/MonsterMove.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 public class MonsterMove : MonoBehaviour
 {
     private Vector3 defaultTransform;
-    public Transform target;
     NavMeshAgent agent;
+    List<GameObject> TagFind = new List<GameObject>();
 
     void Start()
     {
@@ -15,21 +16,41 @@ public class MonsterMove : MonoBehaviour
     }
 
     // Update is called once per frame
+    private GameObject FindNearTag()
+    {
+        string tag = "Player";
+        var objects = GameObject.FindGameObjectsWithTag(tag).ToList();
+
+        var nearObject = objects.OrderBy(obj =>
+        {
+            return Vector3.Distance(transform.position, obj.transform.position);
+        }).FirstOrDefault();
+
+        return nearObject;
+    }
+
     void Update()
     {
         float perimeter = Mathf.Pow(10, 2);     // 기본 범위
 
         float circle = Mathf.Pow(defaultTransform.x - transform.position.x, 2) + Mathf.Pow(defaultTransform.z - transform.position.z, 2);    // 몹의 위치 변환 원
 
-        float navCircle = Mathf.Pow(target.position.x - defaultTransform.x, 2) + Mathf.Pow(target.position.z - defaultTransform.z, 2);    // 타겟과 몹 간의 거리 원
+        if (FindNearTag() != null)
+        {
+            float navCircle = Mathf.Pow(FindNearTag().transform.position.x - defaultTransform.x, 2) + Mathf.Pow(FindNearTag().transform.position.z - defaultTransform.z, 2);    // 타겟과 몹 간의 거리 원
 
-        if (circle >= perimeter)   //몹의 위치가 기본 범위를 벗어 나면
-        {
-            agent.SetDestination(defaultTransform);
-        }
-        else if (perimeter > navCircle)  // 기본 범위가 타겟과 몹 간의 거리 범위보다 클 때
-        {
-            agent.SetDestination(target.position);
+            if (circle >= perimeter)   //몹의 위치가 기본 범위를 벗어 나면
+            {
+                agent.SetDestination(defaultTransform);
+            }
+            else if (perimeter > navCircle)  // 기본 범위가 타겟과 몹 간의 거리 범위보다 클 때
+            {
+                agent.SetDestination(FindNearTag().transform.position);
+            }
+            else
+            {
+                agent.SetDestination(defaultTransform);
+            }
         }
         else
         {
