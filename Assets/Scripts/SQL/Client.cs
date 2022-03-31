ef7e0a9b97e69475504b628ddaf5c834d57cc9d6
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-03-31 17:13:12 +0900

    병합으로 몬스터와 플레이어 연동 확인

--- Assets/Scripts/SQL/Client.cs
+++ Assets/Scripts/SQL/Client.cs
@@ -13,11 +13,14 @@ public class Client : MonoBehaviour
 
     public List<GameObject> Client_MonsterList = new List<GameObject>();// 모든 인게임존재하는 몬스터를 담아둔 클래스
 
+    public float updateSpeed;
+
     [HideInInspector] public Maria5 maria = new Maria5("220.149.12.209", "OpenWorld_MapData", "nagne", "123");
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerDefaultChecking();
+        ClientDefaultSetting();
     }
 
     // Update is called once per frame
@@ -32,6 +35,11 @@ public class Client : MonoBehaviour
         if (ds_json.Tables[0].Rows.Count == 0)
         {
             maria.create_Table(string.Format("INSERT INTO Player_Data(Player_ID) VALUES({0})", "\'" + SystemInfo.deviceUniqueIdentifier + "\'"));
+            DataSet ds_json_intfor = maria.SelectUsingAdapter("SELECT * FROM Player_Data Where Player_ID = '" + SystemInfo.deviceUniqueIdentifier + "\'");
+            PlayerCharctor = Instantiate(PlayerPrefab, PlayerPrefab.transform.position, Quaternion.identity);
+            PlayerCharctor.AddComponent<PlayerInfo>();
+            PlayerCharctor.name = JsonUtility.FromJson<PlayerClass>(ds_json_intfor.Tables[0].Rows[0]["data"].ToString()).Player_name;
+            PlayerCharctor.transform.position = JsonUtility.FromJson<PlayerClass>(ds_json_intfor.Tables[0].Rows[0]["data"].ToString()).PlayerPos;
         }
         else
         {
@@ -40,6 +48,7 @@ public class Client : MonoBehaviour
             PlayerCharctor.name = JsonUtility.FromJson<PlayerClass>(ds_json.Tables[0].Rows[0]["data"].ToString()).Player_name;
             PlayerCharctor.transform.position = JsonUtility.FromJson<PlayerClass>(ds_json.Tables[0].Rows[0]["data"].ToString()).PlayerPos;
         }
+        PlayerCharctor.transform.position = new Vector3(0, 0, 0);
 
         StartCoroutine(Client_UpdateAllData());
     }
@@ -51,7 +60,7 @@ public class Client : MonoBehaviour
             //DownLoad
             DataSet ds_json = maria.SelectUsingAdapter("SELECT * FROM Map1_Monster_Data Where Map_Num = '1'");
             ClientMonsterData = JsonUtility.FromJson<AllMonsterData>(ds_json.Tables[0].Rows[0]["data"].ToString());
-            yield return new WaitForSecondsRealtime(0.1f);
+            yield return new WaitForSecondsRealtime(updateSpeed);
 
             //Upload
             string send_json = JsonUtility.ToJson(new PlayerClass(PlayerCharctor));
@@ -81,7 +90,7 @@ public class Client : MonoBehaviour
     {
         do
         {
-            yield return new WaitForSecondsRealtime(1f);
+            yield return new WaitForSecondsRealtime(updateSpeed);
             for (int i = 0; i < ClientMonsterData.data.Count; i++)
             {
                 if (Client_MonsterList.Find(x => x.gameObject.name == ClientMonsterData.data[i].Mob_name) == null)
@@ -100,7 +109,7 @@ public class Client : MonoBehaviour
     {
         do
         {
-            yield return new WaitForSecondsRealtime(0.05f);
+            yield return new WaitForSecondsRealtime(updateSpeed);
             for (int i = 0; i < ClientMonsterData.data.Count; i++)
             {
                 Client_MonsterList.Find(x => x.gameObject.name == ClientMonsterData.data[i].Mob_name).transform.position = ClientMonsterData.data[i].MobPostion;
