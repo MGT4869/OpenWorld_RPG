83c1538bf79bb683e0067a605a2f399d6f00ed9a
MGT4869 <47241183+MGT4869@users.noreply.github.com>
2022-04-16 18:29:31 +0900

    AniTest 파일추가

---
+++ Assets/Scripts/Sever_Ani_State.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class Sever_Ani_State : MonoBehaviour
+{
+    DataSet severPlayerDataset;
+
+    string Anistate;
+    [HideInInspector] public Maria5 maria = new Maria5("220.149.12.209", "OpenWorld_MapData", "nagne", "123");
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TestAni());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public IEnumerator TestAni()
+    {
+        do
+        {
+            severPlayerDataset = maria.SelectUsingAdapter("SELECT * FROM Player_Data Where Player_ID = '" + "AniTest" + "\'");
+            Anistate = JsonUtility.FromJson<PlayerClass>(severPlayerDataset.Tables[0].Rows[0]["data"].ToString()).AniState;
+            Debug.Log(Anistate);
+            yield return new WaitForSecondsRealtime(0.01f);
+        } while(true);
+    }
+}
